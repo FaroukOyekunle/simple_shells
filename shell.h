0198820ace02de81cf8c1562babf143196c52438
254jumah <jumahtitus@gmail.com>
2023-07-25 11:09:33 -0700

    final changes on shell project

--- shell.h
+++ shell.h
@@ -25,24 +25,4 @@ void _puts(char *str);
 char *_strcpy(char *dest, char *src);
 char *get_cmd(char *line);
 char *_strdup(char *str);
-
-/* Built-in commands */
-int shell_cd(char **args);
-int shell_exit(char **args);
-int shell_env(char **args);
-void env(char **tokenized_command);
-void quit(char **tokenized_command);
-
-/* Utility functions */
-void execute_external_command(char **args);
-void handle_sigint(int signum);
-char *_getenv(char *name);
-
-/* Helper functions */
-void remove_newline(char *str);
-void remove_comment(char *str);
-
-/* Main functions */
-int execute_command(char **args);
-
-#endif /* SHELL_H */
+#endif /* MAIN_H */
